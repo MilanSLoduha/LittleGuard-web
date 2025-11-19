1e4b3d60ae401f6d106f4d39c1213e8b6186db93
Milan Loduha <minoloduha@gmail.com>
2025-11-19 23:53:51 +0100

    fix prisma vercel - proper webpack config 2025

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,18 @@
-// next.config.js
-const { withPrisma } = require('@prisma/nextjs-monorepo-workaround-plugin');
+// next.config.ts
+import type { NextConfig } from "next";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-
+const nextConfig: NextConfig = {
+  webpack(config, { isServer }) {
+    if (isServer) {
+      // Toto úplne vyrieši tvoj problém s rhel-openssl-3.0.x na Verceli
+      config.plugins.push(
+        new (require("webpack").DefinePlugin)({
+          "process.env.PRISMA_SKIP_ENGINE_COPY": JSON.stringify("true"),
+        })
+      );
+    }
+    return config;
+  },
 };
 
-module.exports = withPrisma(nextConfig);
+export default nextConfig;
