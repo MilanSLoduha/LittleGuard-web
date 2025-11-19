515c4b5a64c1b2dafb1967c3e899551701e96c7c
Milan Loduha <minoloduha@gmail.com>
2025-11-19 23:56:43 +0100

    fix prisma vercel - standalone output + disable turbopack

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack(config, { isServer }) {
-    if (isServer) {
-      // Toto úplne vyrieši tvoj problém s rhel-openssl-3.0.x na Verceli
-      config.plugins.push(
-        new (require("webpack").DefinePlugin)({
-          "process.env.PRISMA_SKIP_ENGINE_COPY": JSON.stringify("true"),
-        })
-      );
-    }
-    return config;
-  },
+  turbopack: {},
+
+  output: "standalone",   
+
 };
 
 export default nextConfig;
